9024b5434ed8e4e574eadaabd271c2554fc28a35
zenklot <zenklot11@gmail.com>
2021-10-28 21:01:25 +0700

    Writing handler func

--- server_test.go
+++ server_test.go
@@ -1,6 +1,7 @@
 package learngoweb
 
 import (
+	"fmt"
 	"net/http"
 	"testing"
 )
@@ -8,6 +9,21 @@ import (
 func TestServer(t *testing.T) {
 	server := http.Server{
 		Addr: "localhost:8080",
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
+
+func TestServer_handler(t *testing.T) {
+	var handler http.HandlerFunc = func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Hello World")
+	}
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: handler,
 	}
 
 	err := server.ListenAndServe()
