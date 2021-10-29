cad0bf71f5c599bf01b98cf4ebb1eb0a7ca358f7
zenklot <zenklot11@gmail.com>
2021-10-29 10:33:23 +0700

    add request

--- server_test.go
+++ server_test.go
@@ -57,3 +57,30 @@ func TestServer_serverMux(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServer_request(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, request.Method)
+		fmt.Fprint(writter, request.RequestURI)
+	})
+	mux.HandleFunc("/about/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Halaman About")
+	})
+	mux.HandleFunc("/about/profile/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Halaman Profile")
+	})
+	mux.HandleFunc("/about/contact/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Halaman Contact")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
