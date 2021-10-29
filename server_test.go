10d40a7aa03b9d92313dc05ae06e82669446ca6d
zenklot <zenklot11@gmail.com>
2021-10-29 10:29:54 +0700

    manage many endpoint using serveMux

--- server_test.go
+++ server_test.go
@@ -31,3 +31,29 @@ func TestServer_handler(t *testing.T) {
 		panic(err)
 	}
 }
+
+func TestServer_serverMux(t *testing.T) {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Hello World")
+	})
+	mux.HandleFunc("/about/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Halaman About")
+	})
+	mux.HandleFunc("/about/profile/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Halaman Profile")
+	})
+	mux.HandleFunc("/about/contact/", func(writter http.ResponseWriter, request *http.Request) {
+		fmt.Fprint(writter, "Halaman Contact")
+	})
+
+	server := http.Server{
+		Addr:    "localhost:8080",
+		Handler: mux,
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic(err)
+	}
+}
